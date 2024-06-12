c8785526f56b38708a98311701118f89a4939336
felipe <felipe.turman@gmail.com>
2024-06-11 21:16:07 -0300

    Adicionar arquivos faltantes

---
+++ src/app/features/edit/edit.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../shared/service/products.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../../shared/service/interfaces/product.interface';
+import { FormComponent } from '../../shared/components/form/form.component';
+
+@Component({
+  selector: 'app-edit',
+  standalone: true,
+  imports: [FormComponent],
+  templateUrl: './edit.component.html',
+  styleUrl: './edit.component.scss'
+})
+export class EditComponent {
+  productsService = inject(ProductsService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
+
+
+  product: Product = inject(ActivatedRoute).snapshot.data['product'];
+
+
+  onSubmit(product: Product) {
+
+    this.productsService.put(this.product.id, product).subscribe(() => {
+      this.matSnackBar.open('Produto editado com sucesso!', 'OK');
+
+      this.router.navigateByUrl('/');
+    })
+  }
+}
